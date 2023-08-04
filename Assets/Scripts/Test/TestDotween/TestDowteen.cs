66a989b8ee2d16920eca64ba46bd6f7499e3001d
geliusheng <408963516@qq.com>
2023-08-04 19:51:57 +0800

    test

--- Assets/Scripts/Test/TestDotween/TestDowteen.cs
+++ Assets/Scripts/Test/TestDotween/TestDowteen.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 namespace Test.TestDotween
 {
@@ -9,23 +9,46 @@ namespace Test.TestDotween
     {
         public Transform ts1;
         public Transform ts2;
+        public Text txt;
+        public Image img1;
+        public Image img2;
+        public Camera cam;
         private bool ok = true;
-
-        public void Update()
-        {
-        }
-
         public void OnMouseOver()
         {
             if (ok)
             {
                 ok = false;
                 Debug.Log("====OnMouseOver====ts1=" + ts1.transform + " ts2=" + ts2.transform);
+                // 世界坐标转屏幕坐标
+                Vector3 vec = Camera.main.WorldToScreenPoint(ts1.position);
                 // 朝向
-                ts1.DOLookAt(ts2.position, 2);
+                // ts1.DOLookAt(ts2.position, 2);
                 // 移动到目标点
-                ts1.DOMove(ts2.position, 5);
+                // ts1.DOMove(ts2.position, 5);
+                // 震动/抖动效果
+                // ts1.DOShakeRotation(5);
+                // 动画生成文字
+                txt.DOText("Hello World!", 2);
+                // 颜色渐变
+                txt.DOColor(Color.red, 5);
+                // 渐隐
+                Tweener tweener = txt.DOFade(0.5f, 8);
+                tweener.onComplete = delegate
+                {
+                    // GameObject.Destroy(txt);
+                };
+                // cube1世界坐标转为ui坐标, 移动img1并飘飞
+                // Vector2 pos = Utils.WorldToScreen(ts1.position);
+                // Vector3 vec3 = Utils.ScreenToUI(cam, img2.GetComponent<RectTransform>(), pos);
+                // img2.transform.position = vec3;
+                Vector2 v =  Utils.WorldToScreen(ts1.position);
+                img2.transform.position = v;
             }
+        }
+        public void OnTweenerEnd()
+        {
+            Debug.Log("==OnTweenerEnd==");
         }
     }
 }
---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,91 @@
+﻿/*
+ *  工具类
+ */
+using UnityEngine;
+
+public class Utils {
+    /*
+     *  世界坐标转屏幕坐标
+     *  @pos    世界坐标
+     */
+    public static Vector2 WorldToScreen(Vector3 pos)
+    {
+        return Camera.main.WorldToScreenPoint(pos);
+    }
+    
+    /*
+     *  屏幕坐标转世界坐标
+     *  @pos        屏幕坐标
+     *  @z          距离摄像机Z平面的距离
+     */
+    public static Vector2 ScreenToWorld(Vector2 pos, float z)
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, z));
+    }
+    
+    // RectTransformUtility.WorldToScreenPoint
+    // RectTransformUtility.ScreenPointToWorldPointInRectangle
+    // RectTransformUtility.ScreenPointToLocalPointInRectangle
+    // 上面三个坐标转换的方法使用 Camera 的地方
+    // 当 Canvas renderMode 为 RenderMode.ScreenSpaceCamera、RenderMode.WorldSpace 时 传递参数 canvas.worldCamera
+    // 当 Canvas renderMode 为 RenderMode.ScreenSpaceOverlay 时 传递参数 null
+    
+    /*
+     *  UGUI坐标转屏幕坐标
+     *  @uiCamera    UGUI摄像机
+     *  @pos         UGUI坐标
+     */
+    public static Vector2 UIToScreen(Camera uiCamera, Vector3 pos)
+    {
+        return RectTransformUtility.WorldToScreenPoint(uiCamera, pos);
+    }
+
+    /*
+     *  屏幕坐标转UGUI坐标
+     *  @uiCamera    UGUI摄像机：当Canvas renderMode为RenderMode.ScreenSpaceCamera、RenderMode.WorldSpace 时cam不能空, 为RenderMode.ScreenSpaceOverlay时cam可为空
+     *  @rt          传target.GetComponent<RectTransform>(), 或target.parent.GetComponent<RectTransform>()
+     *  @pos         屏幕坐标
+     *  @ret         返回值  用法：target.transform.position = ret; //target为需要使用的 UI RectTransform
+     */
+    public static Vector3 ScreenToUI(Camera uiCamera, RectTransform rt, Vector2 pos)
+    {
+        Vector3 ret;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, pos, uiCamera, out ret);
+        return ret;
+    }
+    
+    /*
+     *  屏幕坐标转UGUI RectTransform的anchoredPosition
+     *  @uiCamera    UGUI摄像机
+     *  @rt          传target.parent.GetComponent<RectTransform>()
+     *  @pos         屏幕坐标
+     *  @ret         返回值  用法：target.anchoredPosition = ret; //target为需要使用的 UI RectTransform
+     */
+    public static Vector2 ScreenToUILocal(Camera uiCamera, RectTransform parentrt, Vector2 pos)
+    {
+        Vector2 ret;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentrt, pos, uiCamera, out ret);
+        return ret;
+    }
+    
+    public static Vector2 WorldToUI(Vector3 pos, Transform ts)
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+        Vector2 ret;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)ts, screenPos, null, out ret);
+        return ret;
+    }
+    
+    /*
+     * 将世界坐标转换为UGUI坐标 注: 这种算法, cavas不能用填充, 只能用居中
+     */
+    // static public Vector2 WorldToUgui(Vector3 pos)
+    // {
+    //     Vector2 screenPoint = Camera.main.WorldToScreenPoint(pos); //世界坐标转屏幕坐标
+    //     Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+    //     screenPoint -= screenSize / 2;//屏幕坐标变换为以屏幕中心为原点
+    //     Vector2 rectTrans = transform.GetComponent<RectTransform>();
+    //     Vector2 anchorPos = screenPoint / screenSize * rectTrans.sizeDelta; //缩放得到UGUI坐标
+    //     return anchorPos;
+    // }
+}
