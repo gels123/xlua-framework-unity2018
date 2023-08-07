b8a2f4c40058681a6143e7c317335d0fc4acf1d3
geliusheng <408963516@qq.com>
2023-08-07 14:03:02 +0800

    小优化

--- Assets/Scripts/Test/TestDotween/TestDowteen.cs
+++ Assets/Scripts/Test/TestDotween/TestDowteen.cs
@@ -19,36 +19,36 @@ namespace Test.TestDotween
             if (ok)
             {
                 ok = false;
-                Debug.Log("====OnMouseOver====ts1=" + ts1.transform + " ts2=" + ts2.transform);
-                // 世界坐标转屏幕坐标
-                Vector3 vec = Camera.main.WorldToScreenPoint(ts1.position);
-                // 朝向
+                // Debug.Log("====OnMouseOver====ts1=" + ts1.transform + " ts2=" + ts2.transform);
+                // // 朝向
                 // ts1.DOLookAt(ts2.position, 2);
-                // 移动到目标点
+                // // 移动到目标点
                 // ts1.DOMove(ts2.position, 5);
-                // 震动/抖动效果
+                // // 震动/抖动效果
                 // ts1.DOShakeRotation(5);
-                // 动画生成文字
-                txt.DOText("Hello World!", 2);
-                // 颜色渐变
-                txt.DOColor(Color.red, 5);
-                // 渐隐
-                Tweener tweener = txt.DOFade(0.5f, 8);
-                tweener.onComplete = delegate
-                {
-                    // GameObject.Destroy(txt);
-                };
-                // cube1世界坐标转为ui坐标, 移动img1并飘飞
-                // Vector2 pos = Utils.WorldToScreen(ts1.position);
-                // Vector3 vec3 = Utils.ScreenToUI(cam, img2.GetComponent<RectTransform>(), pos);
-                // img2.transform.position = vec3;
-                Vector2 v =  Utils.WorldToScreen(ts1.position);
-                img2.transform.position = v;
+                // // 动画生成文字
+                // txt.DOText("Hello World!", 2);
+                // // 颜色渐变
+                // txt.DOColor(Color.red, 5);
+                // // 渐隐
+                // Tweener tweener = txt.DOFade(0.5f, 8);
+                // tweener.onComplete = () =>
+                // {
+                //     GameObject.Destroy(txt);
+                // };
+                // // img2移动到3d物体处, 然后飘飞
+                // Vector2 v = Utils.WorldToScreen(ts1.position);
+                // img2.transform.position = v;
+                // img2.transform.DOMove(img1.transform.position, 2);
             }
         }
         public void OnTweenerEnd()
         {
             Debug.Log("==OnTweenerEnd==");
+
+            Animation anim = GetComponent<Animation>();
+            anim.Play();
+            anim.CrossFade("xxxx");
         }
     }
 }
